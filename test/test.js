004a2bcabcf0121cb50991770a8b11130165f625
Joshua Wise <joshuathomaswise@gmail.com>
2018-06-25 08:28:08 -0400

    wrote plugin

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 const { expect } = require('chai');
 
+// TODO: make sure it's safe to call .read() on an already-read request with multiple encodings
 describe('decompress()', function () {
 	it('should be tested');
 });
